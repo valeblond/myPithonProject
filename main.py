2dc0eab190efc5ada86e38a0292c3dd98593f500
Valery989 <valera_9796@mail.ru>
2018-10-11 00:56:06 +0200

    Almost over

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from kivy.uix.image import Image
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
+
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
 
 from kivy.config import Config
 Config.set('graphics', 'resizable', '0')
@@ -16,13 +18,15 @@ Config.set('graphics', 'height', '480')
 class MyApp(App):
     def update_label(self):
         self.lbl.text = self.formula
+
     def add_number(self, instance):
         if( self.lbl.text == "0" ):
             self.lbl.text = ""
 
         self.formula += str(instance.text)
         self.update_label()
-    def add_operation(self,instance):
+
+    def add_operation(self, instance):
         if( str(instance.text).lower() == "x" ):
             self.formula += "*"
         else:
@@ -34,25 +38,28 @@ class MyApp(App):
         self.formula = self.lbl.text
 
     def new_photo(self, instance):
-       pass
-       '''
         i = instance.text
+        fl = FloatLayout(size_hint=(1, .09375), size=(640, 60), pos=(0, 0))
+        but4 = Button(text='<--Back', size_hint=(.09375, 1), pos=(0, 0))
+        but5 = Button(text='Bye :D\n  Quit', size_hint=(.09375, 1), pos=(580, 0))
+
         if i in self.dictionary:
             self.image = self.dictionary[i]
-           # print("fefjlslheriuhifarfjvfefjlslheriuhifarfjvfefjlslheriuhifarfjvfefjlslheriuhifarfjvfefjlslheriuhifarfjvfefjlslheriuhifarfjvfefjlslheriuhifarfjvfefjlslheriuhifarfjvfefjlslheriuhifarfjvfefjlslheriuhifarfjvfefjlslheriuhifarfjv\n\n\n\n\n\n\n\n\nfefjlslheriuhifarfjv")
-           # print(self.dictionary[i])
         else:
             raise IndentationError
 
-        bl2 = BoxLayout(orientation='vertical')
-        bl2.add_widget(self.image)
+        self.bl.clear_widgets()
+        fl.add_widget(but4)
+        fl.add_widget(but5)
+        self.bl.add_widget(self.image)
+        self.bl.add_widget(fl)
 
-        return bl2
-        '''
 
-    def build(self):
+    def build(self, instance=None):
         self.image = Image()
         self.formula = ""
+
+        #using of different pics depend on button
         self.dictionary = {
             'Press me, please!!!':     Image(source='Ant_man.jpg', allow_stretch=True, keep_ratio=False),
             'Go and buy \nsome milk!': Image(source='Iron_man.jpg', allow_stretch=True, keep_ratio=False),
@@ -60,7 +67,7 @@ class MyApp(App):
         }
 
        # creating layouts for pics and buttons
-        bl = BoxLayout(orientation='vertical')
+        self.bl = BoxLayout(orientation='vertical')
         rl = RelativeLayout()
         im_hazard = Image(source='Hazard.jpg', allow_stretch=True, keep_ratio=False)
         im_san = Image(source='Sanches.jpg', allow_stretch=True, keep_ratio=False, size_hint=(.234375, 1), pos=(0, 0))
@@ -68,8 +75,9 @@ class MyApp(App):
         but2 = Button(text='Go and buy \nsome milk!', size_hint=(.234375, .5), pos=(300, 120), on_press=self.new_photo)
         but3 = Button(text='You\'re a cool guy :D', size_hint=(.3125, .5), pos=(200, 0), on_press=self.new_photo)
 
-        bl.add_widget(im_hazard)
-        bl.add_widget(rl)
+
+        self.bl.add_widget(im_hazard)
+        self.bl.add_widget(rl)
         rl.add_widget(im_san)
         rl.add_widget(but1)
         rl.add_widget(but2)
@@ -77,7 +85,6 @@ class MyApp(App):
 
 
         #making calculator
-
         fl2 = FloatLayout(size_hint=(.296875, 1), size=(190, 240), pos=(450, 0))
 
         self.lbl = Label(text="0", font_size=15, halign="right", valign="center", pos=(450, 240*.6), size_hint=(1, .4), text_size=(190, 96))
@@ -107,7 +114,7 @@ class MyApp(App):
         fl2.add_widget(gl)
         rl.add_widget(fl2)
 
-        return bl
+        return self.bl
 
 
 
